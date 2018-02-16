a6fabe0713bdee33512f0d1c0cb575098d10297f
Survari <tristanpieper080803@gmail.com>
2018-02-16 12:45:19 +0100

    Added help arguments.

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+#!/bin/python3
+
 from lexer import Lexer
 from parser import Parser
 from executor import Executor
 import sys
 
 def main():
-	fname = sys.argv[1]
-	file = open(fname, "r")
+	if sys.argv[1][0] == "-":
+		if sys.argv[1] == "--help" or sys.argv[1] == "-h" or sys.argv[1] == "-help":
+			print("""call: ./awolip [source file]
+options:
+	-v,--version		- shows the current version of the interpreter
+	-h,-help,--help		- shows this
 
-	Executor().programm(file)
+You have an idea for this project and don't know where to say it?
+Here is our slack: https://bit.ly/2o3FWzv""")
+
+		elif sys.argv[1] == "-v" or sys.argv[1] == "--version":
+			print("v0.0.1")
+	else:
+		fname = sys.argv[1]
+		file = open(fname, "r")
+		Executor().programm(file)
 
 if __name__ == "__main__":
 	main()
